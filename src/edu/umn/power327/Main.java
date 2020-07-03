cc65e00549b11a24df2533db9217ccfa81665512
power327 <power327@d.umn.edu>
2020-07-03 14:30:18 -0500

    Untested: added sha256 hash section, file-to-byte[] section.

--- src/edu/umn/power327/Main.java
+++ src/edu/umn/power327/Main.java
@@ -1,9 +1,13 @@
 package edu.umn.power327;
-//import java.io.IOException;
+import java.io.IOException;
 import java.nio.file.*;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Random;
+import java.util.zip.*;
 
 public class Main {
 
@@ -17,17 +21,46 @@ public class Main {
         int halfSize = range / 2;
 
         Random random = new Random();
+        Deflater compressor = new Deflater();
         Path curfile;
+        String hash;
+        byte[] input, output;
+        long start, stop;
 
         while(range > halfSize) {
             // get a random file and swap it with last in fileList (first out-of-range file)
             Collections.swap(fileList, random.nextInt(range), (range - 1));
             range--;
-            curfile = fileList.get(range); //
+            curfile = fileList.get(range);
             // do compression suite on the first out-of-range file
             // compression things go here
+            // 1) turn file into byte[]
+            try {
+                input = Files.readAllBytes(curfile);
+                output = new byte[input.length + 1];
+                try {
+                    MessageDigest md = MessageDigest.getInstance("SHA-256");
+                    hash = Arrays.toString(md.digest(input));
+//                    System.out.println(hash);
+                } catch (NoSuchAlgorithmException e) {
+                    e.printStackTrace();
+                }
+            } catch (IOException e) {
+                // probably print something if curfile is null, distinct from
+                // when curfile is too large for readAllBytes
+                e.printStackTrace();
+                continue;
+            }
+            // 2) TIMER START
+            start = System.currentTimeMillis();
+            // 3) compressor.setInput(inputByte[]);
+            // 4) compressor.deflate(outputByte[]);
+            // 5) TIMER STOP
+            stop = System.currentTimeMillis();
+            // 6) reset compressor
+            compressor.reset();
 
-            // store the info in the database, or store locally if bad connection
+            // store the info in the database
         }
 //        System.out.println("list size: " + listSize);
     }
