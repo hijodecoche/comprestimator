30f2daac23e2a65b31f9cd4465ca184af326ca17
power327 <power327@d.umn.edu>
2020-07-27 21:28:50 -0500

    Added the db section to Main.java, added deflater.end() after deflater.setInput(input) because apparently that tells the compressor that the input buffer won't receive more input.  To hopefully reduce garbage collection, the output byte array is created once as a 2GB array, which should be enough. The size of the output array isn't necessary for determining compressed file size, so it's fine to have a scratch pad for compressor output.

--- src/edu/umn/power327/Main.java
+++ src/edu/umn/power327/Main.java
@@ -1,17 +1,18 @@
 package edu.umn.power327;
+import edu.umn.power327.database.DBAdapter;
+
 import java.io.IOException;
 import java.nio.file.*;
 import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.Random;
 import java.util.zip.*;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 	// code goes here omg!
         FileEnumerator enumerator = new FileEnumerator();
         // ArrayList should be better than Vector, since ArrayList size grows slower
@@ -20,12 +21,15 @@ public class Main {
         int range = fileList.size(); // will use as a modulus for rng
         int halfSize = range / 2; // this is just for testing to make stuff terminate early
 
+        DBAdapter dbAdapter = new DBAdapter();
+        dbAdapter.createTables();
         Random random = new Random();
         Deflater compressor = new Deflater();
         Path curfile;
         String hash;
-        byte[] input, output;
-        long start, stop;
+        byte[] input, output = new byte[1073741824];
+        long start, stop, start2, stop2;
+        int compressSize;
 
         while(range > halfSize) {
             // get a random file and swap it with last in fileList (first out-of-range file)
@@ -33,18 +37,12 @@ public class Main {
             range--;
             curfile = fileList.get(range);
             // do compression suite on the first out-of-range file
-            // compression things go here
-            // 1) turn file into byte[]
+            // 1) turn file into byte[] and get hash
             try {
                 input = Files.readAllBytes(curfile);
-                output = new byte[input.length + 1];
-                try {
-                    MessageDigest md = MessageDigest.getInstance("SHA-256");
-                    hash = stringify(md.digest(input));
-//                    System.out.println(hash);
-                } catch (NoSuchAlgorithmException e) {
-                    e.printStackTrace();
-                }
+//                output = new byte[input.length + 1];
+                MessageDigest md = MessageDigest.getInstance("SHA-256");
+                hash = stringify(md.digest(input));
             } catch (AccessDeniedException e) {
                 continue;
             } catch (IOException e) {
@@ -56,16 +54,24 @@ public class Main {
             }
             // 2) compressor.setInput(inputByte[]);
             compressor.setInput(input);
+            compressor.finish(); // signals that no new input will enter the buffer
             // 3) TIMER START
             start = System.currentTimeMillis();
             // 4) compressor.deflate(outputByte[]);
-            compressor.deflate(output);
+            compressSize = compressor.deflate(output);
             // 5) TIMER STOP
             stop = System.currentTimeMillis();
             // 6) reset compressor
             compressor.reset();
 
             // store the info in the database
+            try {
+                dbAdapter.insertResult("deflate_results", hash,
+                        "txt", input.length / 1000.0, compressSize / 1000.0, (int)(stop - start) / 1000);
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
 //        System.out.println("list size: " + listSize);
     }
--- src/edu/umn/power327/SingleFileTest.java
+++ src/edu/umn/power327/SingleFileTest.java
@@ -5,12 +5,8 @@ import edu.umn.power327.database.DBAdapter;
 import java.nio.file.*;
 import java.security.MessageDigest;
 import java.sql.SQLException;
-import java.util.Arrays;
 import java.util.Scanner;
 import java.util.zip.Deflater;
-
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
 class SingleFileTest {
 
@@ -39,27 +35,29 @@ class SingleFileTest {
         }
         Deflater compressor = new Deflater();
         String hash;
-        byte[] input, output;
+        byte[] input, output = new byte[1073741824];
         long start, stop;
+        int compressSize;
         // do compression test here
         input = Files.readAllBytes(path);
-        output = new byte[input.length + 1];
+//        output = new byte[input.length + 1];
         MessageDigest md = MessageDigest.getInstance("SHA-256");
         hash = stringify(md.digest(input));
         System.out.println(hash);
         // 2) compressor.setInput(inputByte[]);
         compressor.setInput(input);
+        compressor.finish();
         // 3) TIMER START
         start = System.currentTimeMillis();
         // 4) compressor.deflate(outputByte[]);
-        compressor.deflate(output);
+        compressSize = compressor.deflate(output);
         // 5) TIMER STOP
         stop = System.currentTimeMillis();
 
         // try DB stuff here
         try {
             dbAdapter.insertResult("deflate_results", hash,
-                    "txt", input.length / 1000.0, output.length / 1000.0, (int)(stop - start) / 1000);
+                    "txt", input.length / 1000.0, compressSize / 1000.0, (int)(stop - start) / 1000);
 
         } catch (SQLException e) {
             e.printStackTrace();
