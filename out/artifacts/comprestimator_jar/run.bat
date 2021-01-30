70b711929939872994e37c49e159d191b338d4ee
power327 <power327@d.umn.edu>
2021-01-30 14:27:15 -0600

    Deleted SingleFileTest.java and added bash and batch script files.

---
+++ out/artifacts/comprestimator_jar/run.bat
@@ -0,0 +1,6 @@
+:: test of batch file to call comprestimator
+
+Start java -Xmx6g -jar comprestimator.jar
+
+:: possible compress using `compact filename`
+:: compress only works if successful exit
---
+++ out/artifacts/comprestimator_jar/run.sh
@@ -0,0 +1,3 @@
+#! /bin/sh
+
+java -Xmx6g -jar comprestimator.jar
